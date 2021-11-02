976c7d9aeb8e262a35d88ffad9292778cf657481
Max75013 <maxime.osty@gmail.com>
2021-11-02 18:24:44 +0100

    First UI version

--- Game.java
+++ Game.java
@@ -1,15 +1,84 @@
+import java.util.concurrent.TimeUnit;
+
 public class Game {
     
     private Player[] players;
     private int turn;
 
     private Map map;
+    private UI ui;
 
     public Game(Player[] players) {
         this.players = players;
 
         this.turn = 0;
         this.map = new Map();
+
+        ui = new UI();
+
+        /*GameObject center = new GameObject("./Images/Field.png", 100, 100);
+        center.setPosition(0, 0);
+        ui.add(center);
+
+        GameObject forest = new GameObject("./Images/Forest.png", 100, 100);
+        forest.setPosition(-44, -75);
+        ui.add(forest);*/
+
+        for(Tiles tile : map.getMap()) {
+            GameObject obj = tile.getObject();
+            obj.setInteractable(true);
+            obj.setZindex(1);
+
+            //obj.setOnHoverEnterAction(i -> i.focus(obj));
+            //obj.setOnHoverExitAction(i -> i.unfocus(obj));
+
+            ui.add(tile.getObject());
+        }
+
+        for(int x = 0; x < 11; x++) {
+            for(int y = 0; y < 6; y++) {
+                if((y==0||y==5)&&(x<2||x>8)||(y==1||y==4)&&(x==0||x==10)) continue;
+                
+                GameObject empty = new GameObject(30, 30);
+                empty.setPosition(-44*(5-x), -75*(3-y)+(2*(y%2)-1)*14*((x%2)-((x+1)%2))+38);
+                empty.setZindex(3);
+
+                empty.setInteractable(true);
+
+                empty.setOnHoverEnterAction(i -> i.snap(empty));
+                empty.setOnHoverExitAction(i -> i.exit(empty));
+                empty.setOnMouseClickedAction(i -> i.addNewObject(empty));
+                ui.add(empty);
+            }
+        }
+
+        GameObject button = new GameObject("Images/button.png", 150, 50);
+        button.setInteractable(true);
+        button.setPosition(250, 200);
+        button.setZindex(2);
+
+        button.setOnHoverEnterAction(i -> i.focus(button));
+        button.setOnHoverExitAction(i -> i.unfocus(button));
+        button.setOnMouseClickedAction(i -> i.setNewObject(new GameObject("Images/HouseRed.png", 30, 30)));
+
+        button.setDistanceToSquare();
+        ui.add(button);
+
+        ui.repaint();
+
+        while(ui.isActive()) {
+            ui.analyse();
+            
+            try {
+                TimeUnit.MILLISECONDS.sleep(50);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Game game = new Game(new Player[] { new Player("Moi", false)});
     }
 
 }
--- UI.java
+++ UI.java
@@ -1,42 +1,102 @@
-import javax.imageio.ImageIO;
+import java.util.ArrayList;
+import java.util.List;
 
 import java.awt.*;
 import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.IOException;
 import java.awt.event.*; 
 
 public class UI extends Canvas {
     
-    private static String root = "./Images";
     private static Frame f;
 
-    public static int size = 100;
+    private static int WIDTH = 720;
+    private static int HEIGHT = 480;
+
+    List<GameObject> gameObjects;
+    private GameObject newObject;
+
+    private boolean mouseClicked = false;
 
     public UI () {
         setBackground (Color.CYAN);    
-        setSize(720, 480); 
+        setSize(WIDTH, HEIGHT); 
 
         f = new Frame("Catane");   
         f.add(this);    
     
         f.setLayout(null);    
-        f.setSize(720, 480);    
-        f.setVisible(true);   
+        f.setSize(WIDTH, HEIGHT);    
+        f.setVisible(true); 
+        
+        gameObjects = new ArrayList<>();
+
+        GameObject background = new GameObject("Images/Water.png", WIDTH, HEIGHT);
+        background.setPosition(0, 0);
+        add(background);
         
         // Close event
         f.addWindowListener(new WindowAdapter() {
+            @Override
             public void windowClosing(WindowEvent we) {
                 f.dispose();
             }
         });
+        addMouseListener(new MouseListener() {
+            public void mouseClicked(MouseEvent e) {  
+                if(e.getButton() == 1) 
+                    mouseClicked = true;
+                else if(e.getButton() == 3) {
+                    gameObjects.remove(newObject);
+                    newObject = null;
+                }
+            }  
+            public void mouseEntered(MouseEvent e) { }  
+            public void mouseExited(MouseEvent e) { }  
+            public void mousePressed(MouseEvent e) { }  
+            public void mouseReleased(MouseEvent e) { } 
+        });  
+
+        System.out.println("Ready");
         
+    }
+
+    public void analyse() {
+        Point mousePos = getRelativPosition(MouseInfo.getPointerInfo().getLocation());
+
+        if(newObject != null) newObject.setPosition((int)mousePos.getX(), (int)mousePos.getY());
+
+        GameObject res = null;
+
+        for(GameObject obj : gameObjects) {
+            if(!obj.isInteractable()) continue;
+
+            if(obj.isOn(mousePos)) {
+                if(res == null) res = obj;
+                else {
+                    if(obj.distanceFrom(mousePos) < res.distanceFrom(mousePos)) {
+                        if(res.isHover()) res.onHoverExit(this);
+                        res = obj;
+                    }
+                    else if(obj.isHover()) obj.onHoverExit(this);
+                }
+            }
+            if(obj.isHover()) obj.onHoverExit(this);
+        }
+
+        if(res != null) {
+            if(!res.isHover()) res.onHoverEnter(this);
+            if(mouseClicked) res.onMouseClicked(this);
+        } 
+
+        repaint();
+        mouseClicked = false;
+
+        //System.out.println(mousePos.toString());
     }
 
     public Point getRelativPosition(Point p) {
         Point r = p;
-        r.x = (int)(p.getX() - f.getX() - f.WIDTH/2);
-        r.y = (int)(p.getY() - f.getY() - f.HEIGHT/2);
+        r.setLocation(p.getX() - f.getX(), p.getY() - f.getY());
 
         return r;
     }
@@ -45,60 +105,93 @@ public class UI extends Canvas {
         return f.isActive();
     }
 
-    public static void main(String[] args) {
-        UI test = new UI();
-        Boolean change = false;
-
-        while(test.isActive()) {
-            Point mouse = test.getRelativPosition(MouseInfo.getPointerInfo().getLocation());
-
-            if(mouse.getX() > 50 && mouse.getX() < 150 && mouse.getY() > 50 && mouse.getY() < 150) {
-                test.size = 120;
-                if(!change) {
-                    test.repaint();
-                    change = true;
-                }   
-            }
-            else {
-                test.size = 100;
-                if(change) {
-                    test.repaint();
-                    change = false;
-                }   
-            }
-            //System.out.println(mouse.toString());
-        }
+    public void add(GameObject gameObject) {
+        gameObject.translate(WIDTH/2, HEIGHT/2);
+        this.gameObjects.add(gameObject);
     }
 
     @Override
-    public void paint(Graphics g)    
-    {    
-        BufferedImage originalImage;
-
-        try {
-            originalImage = ImageIO.read(new File(root, "Field.png"));
-            Image field = originalImage.getScaledInstance(size, size, Image.SCALE_SMOOTH);
-            g.drawImage(field, 50, 50, this);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }   
-
-    @Override
     public void update(Graphics g) {
-        g.clearRect(0, 0, 720, 480);
-        BufferedImage originalImage;
+        //g.clearRect(0, 0, 720, 480);        
 
-        try {
-            originalImage = ImageIO.read(new File(root, "Field.png"));
-            Image field = originalImage.getScaledInstance(size, size, Image.SCALE_SMOOTH);
-            g.drawImage(field, 50, 50, this);
-        } catch (IOException e) {
-            e.printStackTrace();
+        BufferedImage frame = new BufferedImage(WIDTH,HEIGHT, BufferedImage.TYPE_INT_ARGB);
+        Graphics2D dis = frame.createGraphics();
+
+        List<GameObject> renderQueu = new ArrayList<>(gameObjects);
+
+        int index = 0;
+        while(!renderQueu.isEmpty()) {
+            int i = 0;
+            while(i < renderQueu.size()) {
+                GameObject gameObject = renderQueu.get(i);
+                if(!gameObject.isVisible()) {
+                    renderQueu.remove(i);
+                    continue;
+                }
+                if(gameObject.getZindex() == index) {
+                    dis.drawImage(gameObject.getImage(), gameObject.getPosX(), gameObject.getPosY(), this);
+                    renderQueu.remove(i);
+                }
+                else i++;
+            }
+            index++;
         }
+
+        g.drawImage(frame, 0, 0, WIDTH, HEIGHT, this);
+
+        //System.out.println("Updated");
     }
 
     public void windowClosing (WindowEvent e) {  
         f.dispose();    
     }   
+
+    public void setNewObject(GameObject object) {
+        Point mousePos = getRelativPosition(MouseInfo.getPointerInfo().getLocation());
+        if(newObject != null) gameObjects.remove(newObject);
+
+        this.newObject = object;
+        object.setPosition((int)mousePos.getX(), (int)mousePos.getY());
+        object.setInteractable(false);
+        object.setZindex(3);
+        add(object);
+    }
+
+    public void addNewObject(GameObject object) {
+        object.setHover(false);
+        newObject = null;
+    }
+
+    public void focus(GameObject object) {
+        object.scale(object.getWidth() + 20, object.getHeight() + 20);
+        object.setHover(true);
+        object.setZindex(2);
+    }
+
+    public void unfocus(GameObject object) {
+        object.scale(object.getWidth() - 20, object.getHeight() - 20);
+        object.setHover(false);
+        object.setZindex(1);
+    } 
+
+    public void snap(GameObject object) {
+        if(object.getImage() != null) return;
+
+        if(newObject == null) return;
+        object.setHover(true);
+
+        object.setImage(newObject.getImage());
+        object.setVisible(true);
+        newObject.setVisible(false);
+    }
+
+    public void exit(GameObject object) {
+        object.setVisible(false);
+        object.setHover(false);
+
+        object.setImage(null);
+
+        if(newObject == null) return;
+        newObject.setVisible(true);
+    }
 }
